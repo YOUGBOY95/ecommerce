0e42504f024061ae03818aada71add0a0bcf216f
Pinard Nathan <86224840+YOUGBOY95@users.noreply.github.com>
2023-08-29 16:37:34 +0200

    modification général

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
+import Image from "next/image"
 
 export function MainNav() {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <img src="/logo.png" alt="Logo" className="h-8 w-8" />
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width="35"
+          height="35"
+        />
         <span className="inline-block text-xl font-bold">
           {siteConfig.name}
         </span>
